0b0ce0f5accdb4efe38e3a470afbc6cdbd31545b
Nikita Shinkov <nikitashinkov22@gmail.com>
2025-11-09 23:55:44 +0500

    верхнее меню

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
-// Пример простого JS-кода
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Страница загружена!');
+let lastScroll = 0;
+const header = document.querySelector('.profile');
+
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll <= 0) {
+    // в начале страницы показываем шапку
+    header.style.transform = 'translateY(0)';
+    return;
+  }
+
+  if (currentScroll > lastScroll) {
+    // скролл вниз → скрываем шапку
+    header.style.transform = 'translateY(-100%)';
+  } else {
+    // скролл вверх → показываем шапку
+    header.style.transform = 'translateY(0)';
+  }
+
+  lastScroll = currentScroll;
 });
+
+
+function adjustMainMargin() {
+  const header = document.querySelector('.profile');
+  const main = document.querySelector('main');
+
+  if (header && main) {
+    const headerHeight = header.offsetHeight; // получаем реальную высоту шапки
+    main.style.marginTop = headerHeight + 'px'; // добавляем margin-top
+  }
+}
+
+// вызываем при загрузке страницы
+window.addEventListener('load', adjustMainMargin);
+
+// вызываем при изменении размера окна, если шапка адаптивная
+window.addEventListener('resize', adjustMainMargin)
+
