269f629067d3148d06f103a541371df5b7549f4d
Nikita Shinkov <nikitashinkov22@gmail.com>
2025-11-10 08:07:07 +0500

    анимация slider

--- script.js
+++ script.js
@@ -45,3 +45,59 @@ window.addEventListener('load', adjustMainMargin);
 // вызываем при изменении размера окна, если шапка адаптивная
 window.addEventListener('resize', adjustMainMargin)
 
+
+
+// Массив с путями к изображениям
+// const images_1 = [
+//   "img/img_example_1_v.png",
+//   "img/img_example_2_v.png",
+//   "img/img_example_3_v.png"
+// ];
+
+// let currentIndex = 0;           // текущий индекс изображения
+// const slider = document.getElementById("slider_1"); 
+
+// // функция для смены изображения
+// function changeImage() {
+//   currentIndex++; 
+//   if (currentIndex >= images_1.length) {
+//     currentIndex = 0;           // возвращаемся к первому изображению
+//   }
+//   slider.src = images_1[currentIndex];
+// }
+
+// // меняем изображение каждые 500 мс
+// setInterval(changeImage, 500);
+
+
+
+// Настройки слайдеров. Добавить для нужной картинки id="slider1" (slider2, slider3 и т.д.)
+const sliders = [
+  {
+    elementId: "slider1",
+    images: ["img/img_example_2_v.png","img/img_example_3_v.png","img/img_example_4_v.png"],
+    displayDuration: 1000
+  },
+  {
+    elementId: "slider2",
+    images: ["img/img_example_3_v.png","img/img_example_2_v.png","img/img_example_1_v.png"],
+    displayDuration: 1500
+  },
+  {
+    elementId: "slider3",
+    images: ["img/img_example_4.png","img/img_example_3.png","img/img_example_2.png"],
+    displayDuration: 2000
+  }
+];
+
+// Создаём слайдеры
+sliders.forEach(slider => {
+  const imgElement = document.getElementById(slider.elementId);
+  let currentIndex = 0;
+
+  setInterval(() => {
+    // Выбираем следующую картинку циклично
+    currentIndex = (currentIndex + 1) % slider.images.length;
+    imgElement.src = slider.images[currentIndex];
+  }, slider.displayDuration);
+});
